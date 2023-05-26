7c64c02bbae2a7cf3c69bfa8ac74df1c2df62d29
Nitishkumar1998 <nitish.marolix@gmail.com>
2023-05-26 11:19:59 +0530

    hotel

---
+++ hotel/home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+
+
+urlpatterns = [
+    path('',home,name='home'),
+    path('login/', login_page , name='login_page'),
+    path('register/', register_page , name='register_page'),
+]
+
+
